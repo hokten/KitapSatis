077b11e44116e16e1973292099b4281fdaa8e706
Hasan ÖKTEN <hokten@gmail.com>
2021-12-31 10:38:38 +0300

    Düzenleme sayfasına kategoriler eklendi

--- kitapduzenle.php
+++ kitapduzenle.php
@@ -16,7 +16,29 @@ include "veritabani.php";
     <form action="kitapduzenle.php" method="post">
         Kitap Adı : <input type="text" name="kadi" value="<?= $kayit["kitapadi"] ?>" /><br />
         Yazarı  : <input type="text" name="yzr" value="<?= $kayit["yazar"] ?>"/><br />
-
+        Kategori :<br />
+        <?php
+        $sql = "SELECT * FROM kitap_kategori WHERE  `kitap_id` = :id ";
+        $sorgu = $baglanti->prepare($sql);
+        $sorgu->bindParam(':id', $_GET["id"], PDO::PARAM_INT);
+        $sorgu->execute();
+        $kitap_kategoriler = $sorgu->fetchAll();
+        $kategoriler=[];
+        foreach($kitap_kategoriler as $sat) {
+            $kategoriler[]=$sat["kategori_id"];
+        }
+        $sql = "SELECT * FROM kategoriler";
+            $sorgu = $baglanti->query($sql);
+            while($satir = $sorgu->fetch()) {
+                $id = $satir["kategori_id"];
+                $ad = $satir["kategori_adi"];
+                $tiklimi="";
+                if (in_array($id, $kategoriler)) {
+                    $tiklimi="checked";
+                }
+                echo "<label><input type='checkbox' name='kategori[]' value='$id' $tiklimi/>$ad</label><br/>";
+            }
+        ?>
         Fiyatı : <input type="text" name="fiy" value="<?= $kayit["fiyat"] ?>" /><br />
         ISBN : <input type="text" name="isbn" value="<?= $kayit["isbn"] ?>" /><br />
         <input type="hidden" name="id" value="<?= $kayit["id"] ?>" /><br />
